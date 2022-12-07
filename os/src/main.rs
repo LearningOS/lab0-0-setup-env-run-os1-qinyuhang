ff3d86e5bf2814e622a2fbf80d3c7a7e2e32abb7
秦宇航 <qinyuhangxiaoxiang@gmail.com>
2022-12-07 03:24:57 +0000

    Pending changes exported from your codespace

---
+++ os/src/main.rs
@@ -0,0 +1,55 @@
+#![no_main]
+#![no_std]
+
+use crate::sbi::shutdown;
+
+core::arch::global_asm!(include_str!("entry.asm"));
+
+mod lang_items;
+
+#[macro_use]
+mod console;
+
+mod sbi;
+
+const SYSCALL_WRITE: usize = 64;
+
+pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
+  syscall(SYSCALL_WRITE, [fd, buffer.as_ptr() as usize, buffer.len()])
+}
+
+const SYSCALL_EXIT: usize = 93;
+
+fn syscall(id: usize, args: [usize; 3]) -> isize {
+    let mut ret;
+    unsafe {
+        core::arch::asm!(
+            "ecall",
+            inlateout("x10") args[0] => ret,
+            in("x11") args[1],
+            in("x12") args[2],
+            in("x17") id,
+        );
+    }
+    ret
+}
+
+pub fn sys_exit(xstate: i32) -> isize {
+    syscall(SYSCALL_EXIT, [xstate as usize, 0, 0])
+}
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| {
+        unsafe { (a as *mut u8).write_volatile(0) }
+    });
+}
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+    clear_bss();
+    shutdown();
+}
